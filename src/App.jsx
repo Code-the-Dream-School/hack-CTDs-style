503c250e3edf3e95f791b5baf02b53e2cf527209
David Greenslade <david.greenslade.cohort2211@gmail.com>
2023-09-03 11:45:01 -0700

    new header and fade effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './App.module.css';
 import Header from './Header.jsx'
 import Search from './Search.jsx'
 import Hero from './Hero.jsx'
@@ -7,6 +8,8 @@ import Links from './Links.jsx'
 import Thanks from './Thanks.jsx'
 import Blog from './Blog.jsx'
 import Footer from './Footer.jsx'
+import Fade from 'react-reveal/Fade';
+
 function App() {
 
   const [searchClicked, setSearchClicked] = React.useState(true);
@@ -17,25 +20,30 @@ function App() {
   }
 
   return (
-    <>
-      {searchClicked ? <Header 
-        setClicked={handleSetSearch}
-      /> : <Search
-        setClicked={handleSetSearch}
-      />}
+    <div className={styles.appContainer}>
+      <header className={styles.stuckHeader}>
+        {searchClicked ? <Header 
+          setClicked={handleSetSearch}
+          /> : <Search
+          setClicked={handleSetSearch}
+          />
+        }
+      </header>
       <main>
-        {/*Current use: image carousel*/}
-        <Hero />
-        {/*Current use: overview information/pitch*/}
-        <Overview/>
-        {/*Current use: Links to other site pages for all users*/}
-        <Links/>
-        <Thanks />
-        <Blog />
+      <Fade>
+          {/*Current use: image carousel*/}
+          <Hero />
+          {/*Current use: overview information/pitch*/}
+          <Overview/>
+          {/*Current use: Links to other site pages for all users*/}
+          <Links/>
+          <Thanks />
+          <Blog />
+        </Fade>
       </main>
       <Footer/>
       {/*Current Use: Footer*/}
-    </>
+    </div>
   )
 }
 
